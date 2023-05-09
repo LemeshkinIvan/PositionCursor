3a6b5bbd28822d0361eefff1768f043ed7ca0e6b
Ivan Lemeshkin <106601642+LemeshkinIvan@users.noreply.github.com>
2023-05-09 18:15:30 +0300

    Add files via upload

---
+++ ChildCursor.cpp
@@ -0,0 +1,93 @@
+﻿// работу выполнил Лемешкин Иван
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	COORD coord;
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi))
+	{
+		cout << "Console screen buffer info failed." << endl;
+	}
+	
+	int buf[] = { 1,2,3,4 };
+	int temp = 0;
+
+	coord.X = atoi(argv[1]);
+	coord.Y = atoi(argv[2]);
+
+	// I'm seriously going to do this
+	COORD ChangePosSec, ChangePosThird, ChangePosFirth;
+
+	ChangePosSec.X = csbi.dwSize.X - coord.X;
+	ChangePosSec.Y = coord.Y;
+
+	ChangePosThird.X = coord.X;
+	ChangePosThird.Y = csbi.dwSize.Y - coord.Y;
+
+	ChangePosFirth.X = csbi.dwSize.X - coord.X;
+	ChangePosFirth.Y = csbi.dwSize.Y - coord.Y;
+
+	// you have to go kill me...
+	for (int i = 0; i < size(buf); i++)
+	{
+		temp = buf[i];
+
+		if (i == 0)
+		{
+			// coordinate default
+			if (SetConsoleCursorPosition(hStdOut, coord))
+			{
+				cout << temp;
+			}
+			else
+			{
+				return GetLastError();
+			}
+		}
+
+		if (i == 1)
+		{
+			if (SetConsoleCursorPosition(hStdOut, ChangePosSec))
+			{
+				cout << temp;
+			}
+			else
+			{
+				return GetLastError();
+			}
+		}
+
+		if (i == 2)
+		{
+			if (SetConsoleCursorPosition(hStdOut, ChangePosThird))
+			{
+				cout << temp;
+			}
+			else
+			{
+				return GetLastError();
+			}
+		}
+
+		if (i == 3)
+		{
+			if (SetConsoleCursorPosition(hStdOut, ChangePosFirth))
+			{
+				cout << temp << endl;
+			}
+			else
+			{
+				return GetLastError();
+			}
+		}
+
+	}
+
+	cin.get();
+}
---
+++ ParentCursor.cpp
@@ -0,0 +1,63 @@
+﻿// работу выполнил Лемешкин Иван
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	string lpszAppName = "I:\\VisualProj\\C++\\ChildCursor\\x64\\Debug\\ChildCursor.exe";
+	string arg1, arg2;
+
+	lpszAppName += " ";
+	
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+
+	si.lpTitle = (char*)"CursorApp";
+	//точка в которой появится левый угол консоли
+	si.dwX = 100;
+	si.dwY = 100;
+	//установил размеры
+	si.dwXSize = 800;
+	si.dwYSize = 700;
+
+	cout << "Instal console buffeer size" << endl << "Enter horizontal value: " << endl;
+	cin >> si.dwXCountChars;
+	cout << "Enter vertical value: " << endl;
+	cin >> si.dwYCountChars;
+
+	// устанавливаем параметры новой консоли
+	si.dwFlags = STARTF_USECOUNTCHARS | STARTF_USEPOSITION | STARTF_USESHOWWINDOW
+		| STARTF_USESIZE;
+	si.wShowWindow = SW_NORMAL;
+	//задаем положение курсора
+	cout << "Enter a indent value\n" << "Indent X:\n";
+	cin >> arg1;
+
+	cout << "Indent Y:\n";
+	cin >> arg2;
+	//осуществляем проверку на отрисовку остальных чисел 
+	int ScreenHalfValueX = si.dwXCountChars / 2 - 1;
+	int ScreenHalfValueY = si.dwYCountChars / 2 - 1;
+
+	if (atoi(arg1.c_str()) > (ScreenHalfValueX) || atoi(arg2.c_str()) > (ScreenHalfValueY))
+	{
+		cout << "Wrong position value!\n";
+		return -1;
+	}
+	// передаем в дочерний процесс (новую консоль)
+	lpszAppName += arg1 + " " + arg2;
+
+	if (!CreateProcess(NULL, (char*)lpszAppName.c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE,
+		NULL, NULL, &si, &pi))
+	{
+		return GetLastError();
+	}
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+}
